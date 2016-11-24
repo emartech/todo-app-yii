20f3cc0e7365e4fc3291c5961bae508971436d3f
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2016-11-24 14:48:36 +0100

    Adding MySQL database to Docker setup

--- www/index.php
+++ www/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'Hello world!';
+echo 'Hello world, I have MySQL ';
+
+$db = new mysqli('172.18.0.1', 'root', 'root', 'mysql');
+echo $db->connect_error;
+
+
+$result = $db->query('select version()');
+print_r($result->fetch_row()[0]);
