ea623f821856fcb0a2749581c8812540f9669f06
Boldizsar Toth <boldizsar.toth@emarsys.com>
2016-11-29 00:03:52 +0100

    Added http request and unit test for it.

--- client/src/app/todo.service.spec.ts
+++ client/src/app/todo.service.spec.ts
@@ -1,16 +1,57 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+
+import { BaseRequestOptions, Response, ResponseOptions, Http } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
 import { TodoService } from './todo.service';
+import { TodoItemFactory } from './misc/todoItemFactory';
 
 describe('TodoService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TodoService]
+      providers: [
+        TodoService,
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          deps: [MockBackend, BaseRequestOptions],
+          useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          }
+        }
+      ],
     });
   });
+
 
   it('should ...', inject([TodoService], (service: TodoService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  it('should return mocked response', inject([TodoService, MockBackend], (testService: TodoService, backend: MockBackend) => {
+
+    // Arrange
+    const baseResponseText = '["list", "of", "todo", "items"]';
+    const baseResponse = new Response(new ResponseOptions({ body: baseResponseText }));
+    backend.connections.subscribe( (c: MockConnection) => c.mockRespond(baseResponse) );
+
+    const testTodoItem = TodoItemFactory.createTodoItem("testTodoTitle", true);
+
+    // Act
+    const request = testService.sendInsertRequest(testTodoItem)
+
+    // Assert
+    request.subscribe( (response: Response) => {
+      expect(response.text()).toBe(baseResponseText);
+      console.log(`Received: ${response.text()}`);
+    });
+
+  }));
+
+
 });
--- client/src/app/todo.service.ts
+++ client/src/app/todo.service.ts
@@ -1,14 +1,22 @@
 import { Component, Injectable, EventEmitter } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
 import { TodoItemFactory } from './misc/todoItemFactory';
 import { TodoItem } from './misc/todoItem';
 
 @Injectable()
 export class TodoService {
 
+  private apiUrl = "http://localhost:1234/api";
+
   private _todoItems: TodoItem[] = [];
   private _todoItems$ = new EventEmitter<TodoItem[]>();
 
-  constructor() { }
+
+  constructor(private http: Http) {
+  }
+
 
   get todoItems() {
     return this._todoItems;
@@ -18,10 +26,13 @@ export class TodoService {
     return this._todoItems$;
   }
 
+
   addTodo(todoTitle: string, todoCompleted = false) {
-    let newTodoItem = TodoItemFactory.createTodoItem(todoTitle, todoCompleted);
+    const newTodoItem = TodoItemFactory.createTodoItem(todoTitle, todoCompleted);
     this._todoItems.push(newTodoItem);
     this.refreshTodoItems();
+
+    this.sendInsertRequest(newTodoItem);
   }
 
   toggleTodo(todoItem: TodoItem) {
@@ -30,12 +41,25 @@ export class TodoService {
   }
 
   removeTodo(todoItem: TodoItem) {
-    let todoItemIndex = this._todoItems.indexOf(todoItem);
+    const todoItemIndex = this._todoItems.indexOf(todoItem);
     this._todoItems.splice(todoItemIndex, 1);
     this.refreshTodoItems();
   }
 
   refreshTodoItems() {
     this._todoItems$.emit(this._todoItems);
+  }
+
+
+  sendInsertRequest(todoItem: TodoItem) {
+    const requestUrl = this.apiUrl + "/Items";
+    const requestBody = JSON.stringify(todoItem);
+    const requestHeaders = new Headers({ 'Content-Type': 'application/json' });
+
+    console.log(`Sent: ${requestBody}`);
+
+    const request = this.http.post(requestUrl, requestBody, requestHeaders);
+
+    return request;
   }
 }
